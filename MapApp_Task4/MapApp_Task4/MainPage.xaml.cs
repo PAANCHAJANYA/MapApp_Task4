b0e4271a4f5996e51b9d025856c5d971c1428bde
KRISHNA PAANCHAJANYA <krishnapaanchajanya1966@gmail.com>
2021-03-29 18:34:55 +0530

    Add project files.

---
+++ MapApp_Task4/MapApp_Task4/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+namespace MapApp_Task4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonCoords_Clicked(object sender, EventArgs e)
+        {
+            if(!double.TryParse(EntryLatitude.Text, out double lat))
+            {
+                return;
+            }
+            if (!double.TryParse(EntryLongitude.Text, out double lng))
+            {
+                return;
+            }
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = EntryName.Text,
+                NavigationMode = NavigationMode.None
+            });
+        }
+    }
+}
